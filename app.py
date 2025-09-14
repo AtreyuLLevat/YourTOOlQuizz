b6030e13f1c9578d64ae97613df0077aed88826e
AtreyuLLevat <atreyulaguilera@gmail.com>
2025-09-14 18:42:20 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -33,8 +33,9 @@ def create_app():
 
     # Configuración de correo
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USE_TLS'] = False
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
