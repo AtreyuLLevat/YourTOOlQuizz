1676f51125ea569a4976cf8fddecb14f5f5d3732
AtreyuLLevat <atreyulaguilera@gmail.com>
2025-10-11 12:04:22 +0200

    Add files via upload

--- static/java/quizzproductividad.js
+++ static/java/quizzproductividad.js
@@ -77,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (currentQuestion === totalQuestions - 1) {
       nextBtn.textContent = "Enviando...";
       nextBtn.disabled = true;
-setTimeout(() => window.location.href = "{{ url_for('servicio') }}", 300);
+setTimeout(() => window.location.href = "/Servicio_1", 300);
 
     } else {
       showQuestion(currentQuestion + 1);
