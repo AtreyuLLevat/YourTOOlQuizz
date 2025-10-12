3b923d6f62def84fbca319eb838d730ba7217209
AtreyuLLevat <atreyulaguilera@gmail.com>
2025-10-12 20:08:34 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,9 +9,5 @@ if __name__ == "__main__":
 
 from app import create_app
 
-app = create_app()
 
-# opcional: solo para debug local, Gunicorn no usará esto
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
 
