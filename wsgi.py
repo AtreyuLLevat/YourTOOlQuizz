88017c461919bc2b024e648e32e5ab0b13e8bcc6
AtreyuLLevat <atreyulaguilera@gmail.com>
2025-10-12 20:07:59 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ app = create_app()
 # opcional: solo para debug local, Gunicorn no usará esto
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
-=======
+
 from app import create_app
 
 app = create_app()
@@ -14,4 +14,4 @@ app = create_app()
 # opcional: solo para debug local, Gunicorn no usará esto
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
->>>>>>> ec1e43cfb7f709d6f69ef6f58ce66c05937cf404
+
