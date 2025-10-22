62c458b199d9035ded79c5e683594bcd56b07c18
AtreyuLLevat <atreyulaguilera@gmail.com>
2025-10-22 18:04:57 +0200

    Add files via upload

--- static/java/supabaseClient.js
+++ static/java/supabaseClient.js
@@ -1,8 +1,8 @@
-// supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// ⚠️ Reemplaza con tus datos reales
-const SUPABASE_URL = 'https://TU-PROYECTO.supabase.co';
-const SUPABASE_ANON_KEY = 'TU_SUPABASE_ANON_KEY';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { createClient } from "./libs/supabase.min.js";
+
+const SUPABASE_URL = "https://TU-PROJECTO.supabase.co";
+const SUPABASE_KEY = "TU-API-KEY";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
