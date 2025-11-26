200b809896427df24d3799828bd754eb209a9d51
AtreyuLLevat <atreyulaguilera@gmail.com>
2025-11-26 18:51:00 +0100

    Add files via upload

--- extensions.py
+++ extensions.py
@@ -4,10 +4,18 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
 from flask_socketio import SocketIO
+from supabase import create_client
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
 mail = Mail()
 socketio = SocketIO(cors_allowed_origins="*")   # SIN async_mode
+
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")  # o SERVICE_ROLE_KEY si es backend seguro
+SUPABASE_ANON_KEY = os.getenv("SUPABASE_ANON_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
 
