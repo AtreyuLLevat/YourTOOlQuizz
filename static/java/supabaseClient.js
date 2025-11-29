cfa38fa01c50d24103dfa44c3276160c9fe4f78c
AtreyuLLevat <atreyulaguilera@gmail.com>
2025-11-29 16:39:14 +0100

    Add files via upload

--- static/java/supabaseClient.js
+++ static/java/supabaseClient.js
@@ -1,5 +1,5 @@
 
-  import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.86.0/dist/umd/supabase.min.js";
+  import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.86.0/dist/supabase.js";
 
   const SUPABASE_URL = "db.ouoodvqsezartigpzwke.supabase.co";
   const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91b29kdnFzZXphcnRpZ3B6d2tlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMzI3NDUsImV4cCI6MjA3MTYwODc0NX0.LiS7sXDRc2KA0uYr3QXp0QM3C_PAmL1pVki0g0cp2wU";
