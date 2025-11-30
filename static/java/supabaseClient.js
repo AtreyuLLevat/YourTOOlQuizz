8f97e7adff5ff2eecdeba9bfe274495637edab25
AtreyuLLevat <atreyulaguilera@gmail.com>
2025-11-30 18:51:27 +0100

    Refactor supabaseClient.js for clarity and updates

--- static/java/supabaseClient.js
+++ static/java/supabaseClient.js
@@ -7,7 +7,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // ⚠️ IMPORTANTE: NO pongas la anon key directa aquí en producción.
 // Flask debe inyectarla desde el backend.
 const supabaseUrl = "https://db.ouoodvqsezartigpzwke.supabase.co";
-const SUPABASE_KEY = "{{ SUPABASE_KEY }}";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91b29kdnFzZXphcnRpZ3B6d2tlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMzI3NDUsImV4cCI6MjA3MTYwODc0NX0.LiS7sXDRc2KA0uYr3QXp0QM3C_PAmL1pVki0g0cp2wU";
 
 // Crear cliente
 export const supabase = createClient(supabaseUrl, SUPABASE_KEY);
@@ -20,3 +20,4 @@ supabase.auth.getSession().then(({ data, error }) => {
         console.log("✅ Supabase conectado");
     }
 });
+
